a705e1db730c08a97badabd12969136b847869a3
vkesary <vkesary@IM-RT-LP-036.INNOMINDSBANG.COM>
2018-08-20 12:11:11 +0530

    commited by venky

---
+++ DesingPatternsPluralSight/src/com/designpatterns/creationalpatterns/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.designpatterns.creationalpatterns;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to git");
+	}
+
+}
