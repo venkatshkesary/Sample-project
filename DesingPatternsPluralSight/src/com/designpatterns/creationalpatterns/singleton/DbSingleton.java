8a99ea00c5dfdc48888a722cad4fdd2986314592
vkesary <vkesary@IM-RT-LP-036.INNOMINDSBANG.COM>
2018-08-20 12:36:09 +0530

    lagy loading

--- DesingPatternsPluralSight/src/com/designpatterns/creationalpatterns/singleton/DbSingleton.java
+++ DesingPatternsPluralSight/src/com/designpatterns/creationalpatterns/singleton/DbSingleton.java
@@ -2,13 +2,16 @@ package com.designpatterns.creationalpatterns.singleton;
 
 public class DbSingleton {
 	
-	private static DbSingleton instance=new DbSingleton();
+	private static DbSingleton instance=null;
 	
 	private DbSingleton() {
 		
 	}
 	
 	public static DbSingleton getInstace() {
+		if(instance==null)
+			instance=new DbSingleton();
+		
 		return instance;
 	}
 
