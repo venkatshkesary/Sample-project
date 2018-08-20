9f407337d3737a95511eebd68646a29a04b01a83
vkesary <vkesary@IM-RT-LP-036.INNOMINDSBANG.COM>
2018-08-20 12:51:56 +0530

    thread safe singleton

--- DesingPatternsPluralSight/src/com/designpatterns/creationalpatterns/singleton/DbSingleton.java
+++ DesingPatternsPluralSight/src/com/designpatterns/creationalpatterns/singleton/DbSingleton.java
@@ -1,16 +1,26 @@
 package com.designpatterns.creationalpatterns.singleton;
 
+import org.w3c.dom.ranges.RangeException;
+
 public class DbSingleton {
 	
-	private static DbSingleton instance=null;
+	private static volatile DbSingleton instance=null;
 	
 	private DbSingleton() {
+		if(instance!=null) {
+			throw new RuntimeException("Use getInstace() to create object");
+		}
 		
 	}
 	
 	public static DbSingleton getInstace() {
-		if(instance==null)
-			instance=new DbSingleton();
+		if(instance==null) {
+			synchronized (DbSingleton.class) {
+				if(instance==null)
+					instance=new DbSingleton();
+			}
+		
+		}
 		
 		return instance;
 	}
